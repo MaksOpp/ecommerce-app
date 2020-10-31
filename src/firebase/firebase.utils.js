b54f174e87d7158517926d21d1d55b07b463bd32
unknown <maks.operlejn@gmail.com>
2020-10-31 23:50:38 +0100

    Added loading spinner

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -21,13 +21,13 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     const snapShot = await userRef.get();
 
-    if(!snapShot.exists) {
-        const {displayName, email} = userAuth;
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
 
         const createdAt = new Date();
 
         try {
-            await userRef.set({displayName, email, createdAt, ...additionalData})
+            await userRef.set({ displayName, email, createdAt, ...additionalData })
         } catch (error) {
             console.log('Error creating user', error.message);
         }
@@ -35,16 +35,52 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     return userRef;
 
-    
+
 }
 
 firebase.initializeApp(config);
+
+
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach((obj) => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map((doc) => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items,
+        };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+};
+
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
